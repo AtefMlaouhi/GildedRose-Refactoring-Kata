81c4b8e826688e4fdc4eebd5896e8829b598eaa8
Atef Mlaouhi <atef.mlaouhi@outlook.com>
2019-01-25 01:21:08 +0100

    Add GildedRose Legacy code

---
+++ GildedRoseRefactoringKata.Tests/GildedRoseTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace GildedRoseRefactoringKata
+{
+    [TestFixture]
+    public class GildedRoseTest
+    {
+        [Test]
+        public void Should_Be_True()
+        {
+            Assert.True(true);
+        }
+    }
+}
