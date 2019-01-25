f4345462caab26d5d5ed05b7e602ecc894e71c0e
Atef Mlaouhi <atef.mlaouhi@outlook.com>
2019-01-25 01:27:39 +0100

    case test Given_Quality_Should_Be_Positive failed must fix after refact

--- GildedRoseRefactoringKata.Tests/GildedRoseTest.cs
+++ GildedRoseRefactoringKata.Tests/GildedRoseTest.cs
@@ -7,9 +7,13 @@ namespace GildedRoseRefactoringKata
     public class GildedRoseTest
     {
         [Test]
-        public void Should_Be_True()
+        public void Given_Quality_Should_Be_Positive()
         {
-            Assert.True(true);
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 5, Quality = -10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.GreaterOrEqual(Items[0].Quality, 0);
         }
+
     }
 }
