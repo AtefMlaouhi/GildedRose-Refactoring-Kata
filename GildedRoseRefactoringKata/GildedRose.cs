b7ab58a94c67be0df1563d2aa2543273a0113b58
Atef Mlaouhi <atef.mlaouhi@outlook.com>
2019-01-25 11:51:36 +0100

    extract default case

--- GildedRoseRefactoringKata/GildedRose.cs
+++ GildedRoseRefactoringKata/GildedRose.cs
@@ -13,10 +13,8 @@ namespace GildedRoseRefactoringKata
 
         public void UpdateQuality()
         {
-            bool condition = false;
             for (var i = 0; i < Items.Count; i++)
             {
-
                 switch (Items[i].Name)
                 {
                     case "Sulfuras, Hand of Ragnaros":
@@ -66,89 +64,25 @@ namespace GildedRoseRefactoringKata
 
                             if (Items[i].SellIn < 0)
                             {
-                                this.SetQuality(i, -Items[i].Quality);
+                                Items[i].Quality = 0;
                             }
                             break;
                         }
                     default:
                         {
-                            condition = Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert";
-
-                            if (condition)
+                            if (Items[i].Quality > 0)
                             {
-                                if (Items[i].Quality > 0)
-                                {
-                                    condition = Items[i].Name != "Sulfuras, Hand of Ragnaros";
-
-                                    if (condition)
-                                    {
-                                        this.SetQuality(i, -1);
-                                    }
-                                }
-                            }
-                            else
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    this.SetQuality(i, 1);
-                                }
+                                this.SetQuality(i, -1);
                             }
 
-                            condition = Items[i].Name == "Backstage passes to a TAFKAL80ETC concert";
-
-                            if (condition)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    if (Items[i].SellIn < 11)
-                                    {
-                                        if (Items[i].Quality < 50)
-                                        {
-                                            this.SetQuality(i, 1);
-                                        }
-                                    }
-
-                                    if (Items[i].SellIn < 6)
-                                    {
-                                        if (Items[i].Quality < 50)
-                                        {
-                                            this.SetQuality(i, 1);
-                                        }
-                                    }
-                                }
-                            }
-
-                            condition = Items[i].Name != "Sulfuras, Hand of Ragnaros";
-
-                            if (condition)
-                            {
-                                this.SetSellIn(i);
-                            }
+                            this.SetSellIn(i);
 
                             if (Items[i].SellIn < 0)
                             {
-                                condition = Items[i].Name != "Aged Brie";
-
-                                if (condition)
+                                if (Items[i].Quality > 0)
                                 {
-                                    condition = Items[i].Name != "Backstage passes to a TAFKAL80ETC concert";
 
-                                    if (condition)
-                                    {
-                                        if (Items[i].Quality > 0)
-                                        {
-                                            condition = Items[i].Name != "Sulfuras, Hand of Ragnaros";
-
-                                            if (condition)
-                                            {
-                                                this.SetQuality(i, -1);
-                                            }
-                                        }
-                                    }
-                                    else
-                                    {
-                                        this.SetQuality(i, -Items[i].Quality);
-                                    }
+                                    this.SetQuality(i, -1);
                                 }
                             }
                             break;
