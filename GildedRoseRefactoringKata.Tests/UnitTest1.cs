b57f63dca51d15968007e8996e3f887a1975fe33
Atef Mlaouhi <atef.mlaouhi@outlook.com>
2019-01-25 01:03:22 +0100

    Add readme Add badeg license Add badge travis Add config .travis.yml Add badge bettercodehub Add badge codeclimate Add Solution GildedRoseRefactoringKata Add Project GildedRoseRefactoringKata Add Project Test GildedRoseRefactoringKata.Tests

---
+++ GildedRoseRefactoringKata.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿namespace GildedRoseRefactoringKata.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.True(true);
+        }
+    }
+}
