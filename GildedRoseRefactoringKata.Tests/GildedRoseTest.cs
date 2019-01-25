b089120f51fe70e0f15fa2e98ceabd2a58e6f010
Atef Mlaouhi <atef.mlaouhi@outlook.com>
2019-01-25 01:45:01 +0100

    test case for Backstage passes: When_UpdateQuality_Backstage_Passes_Quality_Should_Be_Increases_By_2_If_SellIn_Equal_10_Or_Less When_UpdateQuality_Backstage_Passes_Quality_Should_Be_Increases_By_3_If_SellIn_Equal_5_Or_Less When_UpdateQuality_Backstage_Passes_Quality_Should_Be_Equal_0_If_SellIn_Less_than_Zero

--- GildedRoseRefactoringKata.Tests/GildedRoseTest.cs
+++ GildedRoseRefactoringKata.Tests/GildedRoseTest.cs
@@ -138,5 +138,37 @@ namespace GildedRoseRefactoringKata
             Assert.AreEqual(Items[0].Quality, 80);
         }
 
+        [Test, Description("Backstage passes")]
+        public void When_UpdateQuality_Backstage_Passes_Quality_Should_Be_Increases_By_2_If_SellIn_Equal_10_Or_Less()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 8, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.GreaterOrEqual(Items[0].Quality, 12);
+            app.UpdateQuality();
+            Assert.GreaterOrEqual(Items[0].Quality, 14);
+        }
+
+        [Test, Description("Backstage passes")]
+        public void When_UpdateQuality_Backstage_Passes_Quality_Should_Be_Increases_By_3_If_SellIn_Equal_5_Or_Less()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.GreaterOrEqual(Items[0].Quality, 13);
+            app.UpdateQuality();
+            Assert.GreaterOrEqual(Items[0].Quality, 15);
+        }
+
+        [Test, Description("Backstage passes")]
+        public void When_UpdateQuality_Backstage_Passes_Quality_Should_Be_Equal_0_If_SellIn_Less_than_Zero()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 1, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.GreaterOrEqual(Items[0].Quality, 13);
+            app.UpdateQuality();
+            Assert.GreaterOrEqual(Items[0].Quality, 0);
+        }
     }
 }
