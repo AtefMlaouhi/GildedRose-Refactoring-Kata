12a48f296f5d4135b2c40038dcf26280f6d92d12
Atef Mlaouhi <atef.mlaouhi@outlook.com>
2019-02-01 00:21:30 +0100

    Correct file name

--- GildedRoseRefactoringKata/Program.cs
+++ GildedRoseRefactoringKata/Program.cs
@@ -8,36 +8,18 @@ namespace GildedRoseRefactoringKata
         public static void Main(string[] args)
         {
             Console.WriteLine("OMGHAI!");
-
             IList<Item> Items = new List<Item>{
                 new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
                 new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
                 new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
                 new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
                 new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 15,
-                    Quality = 20
-                },
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 10,
-                    Quality = 49
-                },
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 5,
-                    Quality = 49
-                },
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20},
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 49},
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5,Quality = 49},
 				// this conjured item does not work properly yet
 				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-            };
-
-            var app = new GildedRose();
+            }; var app = new GildedRose();
             app.SetItems(Items);
             app.Products = new Product[] { Product.Aged_Brie, Product.Backstage_Passes, Product.Sulfuras};
             app.TypeServices = new Type[] { DefaultProductService.ServicesType, AgedBrieProductService.ServicesType, BackstagePassesProductService.ServicesType, SulfurasProductService.ServicesType };
